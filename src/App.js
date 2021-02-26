ca8c60a9455239d7493c39f73d841adb3125d222
Wendellsg <wendell.silva.guimaraes@gmail.com>
2021-02-26 18:37:42 -0300

    Finalizado o tobim cagão

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import toby from './assets/toby.jpeg'
+import pum from './assets/HumanFart.mp3'
 
 function App() {
+
+  const texto = 'Toby'
+
+  const [msg, setMsg] = useState('');
+
+  const [count, setCount] = useState(0);
+
+  const [caixa, setcaixa] = useState('')
+
+
+
+  const audio = new Audio(pum);
+
+
+  function handleclick(){
+    setCount(count + 1)
+    audio.play();
+
+    if(count >= 5){
+      setcaixa('a')
+      setMsg("Ai minha barriguinha");
+    }
+
+    if(count >= 10){
+      setMsg("Não aguento mais cagaaaar");
+    }
+    if(count >= 15){
+    setMsg("É muito cocôoooooo");
+    }
+  }
+
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className={`msg ${caixa}`}>{msg}</p>
+      <img className="toby" src={toby} alt=""/>
+      <h1 className='titulo'>{texto} cagou {count} vezes</h1>
+
+      <p>Click e faça o Toby cagar</p>
+       <button onClick={handleclick}>
+        
+       </button>
+
+
+
     </div>
+    
   );
 }
 
